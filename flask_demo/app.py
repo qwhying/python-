fe37482aa14f363f79661196a9ae026c860ccf2e
qwhying <woshiqiwanga@163.com>
2019-07-02 22:10:00 +0800

    20190702

--- flask_demo/app.py
+++ flask_demo/app.py
@@ -2,11 +2,14 @@
 from flask import Flask,render_template
 from flask import request
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 #from flask_script import Manager
 #需要传入__name__，作用是为了确定资源所在的路径
 app = Flask(__name__)
 #manager=Manager(app)
 bootstrap=Bootstrap(app)
+moment=Moment(app)
+app.config['SECRET_KEY']='hard to guess string'
 # 定义路由及视图函数
 #通过装饰器实现的
 @app.route('/')
@@ -14,7 +17,7 @@ def index():
     #user_agent=request.headers.get('User-Agent')
     #return '<p>Your browser is %s</p>'% user_agent#返回响应可以有三个参数第二
 #个是状态码，第三个是由首部组成的字典
-    return render_template('index.html')
+    return render_template('index.html',current_time=datetime.utcnow())
 @app.route('/user/<name>')
 def user(name):
     #return '<h1>Hello,%s!</h1>'% name
